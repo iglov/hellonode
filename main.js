e726eb1696a89cebc62d453a3a824f88fee59ec4
iglov <iglov@xakep.ru>
2018-06-07 18:58:22 +0500

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hop hey la-la-ley, ofigenniy ya kakoy [v2]\n");
+  response.end("PREVED LEHA!\n");
 });
 
 // listen on localhost:8000
